4479d1a2d4abe2a8c6ae3e8a0e6e5026a9bda8ea
x-1337-x <g1thub_acc@protonmail.com>
2020-06-04 21:40:18 +0200

    log/items/preloader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 const App = () => {
 	useEffect(() => {
 		M.AutoInit();
 	});
-	return <div className='App'>App</div>;
+	return (
+		<Fragment>
+			<SearchBar />
+			<div className='container'>
+				<Logs />
+			</div>
+		</Fragment>
+	);
 };
 
 export default App;
