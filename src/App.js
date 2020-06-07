04cd3baaf54eedd8f358618f5eda2d4403e80a37
x-1337-x <g1thub_acc@protonmail.com>
2020-06-07 12:22:54 +0200

    techs modal/item

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 import AddBtn from './components/layout/AddBtn';
 
 const App = () => {
@@ -18,6 +21,9 @@ const App = () => {
 			<div className='container'>
 				<AddBtn />
 				<AddLogModal />
+				<EditLogModal />
+				<AddTechModal />
+				<TechListModal />
 				<Logs />
 			</div>
 		</Fragment>
