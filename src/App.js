731fa632fcb9a8ecf8a3a9f9264e3981cc726567
x-1337-x <g1thub_acc@protonmail.com>
2020-06-07 10:15:59 +0200

    AddLogModal

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddLogModal from './components/logs/AddLogModal';
+import AddBtn from './components/layout/AddBtn';
 
 const App = () => {
 	useEffect(() => {
@@ -14,6 +16,8 @@ const App = () => {
 		<Fragment>
 			<SearchBar />
 			<div className='container'>
+				<AddBtn />
+				<AddLogModal />
 				<Logs />
 			</div>
 		</Fragment>
