b24f3419ecd198f8d51215c50cf8cee4c5630beb
x-1337-x <g1thub_acc@protonmail.com>
2020-06-08 11:11:07 +0200

    Delete Tech

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -48,6 +48,26 @@ export const addTech = (tech) => async (dispatch) => {
 	}
 };
 
+export const deleteTech = (id) => async (dispatch) => {
+	try {
+		setLoading();
+
+		await fetch(`/techs/${id}`, {
+			method: 'DELETE',
+		});
+
+		dispatch({
+			type: DELETE_TECH,
+			payload: id,
+		});
+	} catch (error) {
+		dispatch({
+			type: TECHS_ERROR,
+			payload: error.response.statusText,
+		});
+	}
+};
+
 export const setLoading = () => {
 	return {
 		type: SET_LOADING,
--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { deleteTech } from '../../actions/techActions';
 
-const TechItem = ({ tech }) => {
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const TechItem = ({ tech, deleteTech }) => {
+	const onDelete = () => {
+		deleteTech(tech.id);
+		M.toast({ html: `${tech.firstName} ${tech.lastName} deleted` });
+	};
+
 	return (
 		<li className='collection-item'>
 			<div>
 				{tech.firstName} {tech.lastName}
-				<a href='#!' className='secondary-content'>
+				<a href='#!' className='secondary-content' onClick={() => onDelete()}>
 					<i className='material-icons grey-text'>delete</i>
 				</a>
 			</div>
@@ -16,6 +25,7 @@ const TechItem = ({ tech }) => {
 
 TechItem.propTypes = {
 	tech: PropTypes.object.isRequired,
+	deleteTech: PropTypes.func.isRequired,
 };
 
-export default TechItem;
+export default connect(null, { deleteTech })(TechItem);
