0b29499313458dd9dbb0d55504c2eeed8724a178
x-1337-x <g1thub_acc@protonmail.com>
2020-06-08 10:58:22 +0200

    Add Tech

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -25,6 +25,29 @@ export const getTechs = () => async (dispatch) => {
 	}
 };
 
+export const addTech = (tech) => async (dispatch) => {
+	try {
+		setLoading();
+
+		const res = await fetch('/techs', {
+			method: 'POST',
+			body: JSON.stringify(tech),
+			headers: { 'Content-Type': 'application/json' },
+		});
+		const data = await res.json();
+
+		dispatch({
+			type: ADD_TECH,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: TECHS_ERROR,
+			payload: error.response.statusText,
+		});
+	}
+};
+
 export const setLoading = () => {
 	return {
 		type: SET_LOADING,
